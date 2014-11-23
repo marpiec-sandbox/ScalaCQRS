41ad11937f6069f3e78e40e44c0edcc10cd14f0c
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2014-11-23 22:20:20 +0100

    Parallel test for memory uid generator

---
+++ TODO.md
@@ -0,0 +1,11 @@
+Tests to create for memory implementation
+
+1. Getting aggregate with version lower than 1
+2. Getting not yet created aggregate
+3. Getting events _from_ version
+4. Add event when other event was added concurrently (both modification event and deletion event - and join the code)
+
+5. Scrambled internal state...
+6. Debug mode in logger enabled
+
+7. Multithreaded tests?
--- memory/src/test/scala/pl/mpieciukiewicz/scalacqrs/MemorySequentialUIDGeneratorSpec.scala
+++ memory/src/test/scala/pl/mpieciukiewicz/scalacqrs/MemorySequentialUIDGeneratorSpec.scala
@@ -5,7 +5,8 @@ import org.fest.assertions.api.Assertions.assertThat
 import org.scalatest.{FeatureSpec, GivenWhenThen}
 import pl.mpieciukiewicz.scalacqrs.memoryimpl.MemorySequentialUIDGenerator
 
-import scala.collection.mutable
+import scala.collection.parallel.{ForkJoinTaskSupport}
+import scala.collection.parallel.immutable.{ParRange}
 
 class MemorySequentialUIDGeneratorSpec extends FeatureSpec with GivenWhenThen {
 
@@ -21,17 +22,22 @@ class MemorySequentialUIDGeneratorSpec extends FeatureSpec with GivenWhenThen {
 
 
       When("Menu UIDs are generated")
-      val generatedUids = mutable.HashSet[UID]()
 
-      for (i <- 0 until generations) {
-        generatedUids += generator.nextUID
-      }
+      val parRange: ParRange = Range(0, generations).par
+      parRange.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(10))
+      val generatedUIDs = parRange.map(el => generator.nextUID).toSet
+
 
       Then("Number of unique generated UID is equal to number of generations")
 
-      assertThat(generatedUids.size).isEqualTo(generations)
+      assertThat(generatedUIDs.size).isEqualTo(generations)
 
     }
+
+
+  }
+
+  def runInParallel(count: Int)(block: () => Set[UID]): Unit = {
 
 
   }
