0768a0add76adf448026a5f80a0af91fa692ab86
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-02-20 10:36:58 +0100

    Improved command store code

--- postgres/src/main/scala/pl/mpieciukiewicz/scalacqrs/postgresimpl/PostgresCommandStore.scala
+++ postgres/src/main/scala/pl/mpieciukiewicz/scalacqrs/postgresimpl/PostgresCommandStore.scala
@@ -21,49 +21,30 @@ class PostgresCommandStore(dbDataSource: DataSource, serializer: ObjectSerialize
 
   new CommandSchemaInitializer().initSchema()
 
-  override def addCommand(
-                           commandId: CommandId, userId: UserId,
-                           command: AnyRef): Unit = {
-    val
-    connection =
-      dbDataSource.getConnection
-    val statement = connection.prepareStatement(INSERT_COMMAND)
-    statement.setLong(1, commandId.uid)
-    statement.setLong(2, userId.uid)
-    statement.setString(3, command.getClass.getName)
-    statement.setInt(4, 0)
-    statement.setString(5, serializer.toJson(command))
-    statement.execute()
-    statement.close()
-    connection.close()
+  override def addCommand(commandId: CommandId, userId: UserId, command: AnyRef): Unit = {
+    executeStatement(INSERT_COMMAND) {statement =>
+      statement.setLong(1, commandId.uid)
+      statement.setLong(2, userId.uid)
+      statement.setString(3, command.getClass.getName)
+      statement.setInt(4, 0)
+      statement.setString(5, serializer.toJson(command))
+    }
   }
 
   override def getCommandById(commandId: CommandId): CommandRow = {
-    val connection = dbDataSource.getConnection
-    val statement = connection.prepareStatement(SELECT_COMMAND_BY_ID)
-    statement.setLong(1, commandId.uid)
-    val command = getCommands(connection, statement).head
-    statement.close()
-    connection.close()
-    command
-  }
-
-  private def getCommands[T](connection: Connection, statement: PreparedStatement): List[CommandRow] = {
-    val resultSet = statement.executeQuery()
-
-    var eventsRows = List[CommandRow]()
-    while (resultSet.next()) {
-      val commandRow = CommandRow(
-        CommandId(resultSet.getLong(1)),
-        UserId(resultSet.getLong(2)),
-        resultSet.getTimestamp(3).toInstant,
-        serializer.fromJson(resultSet.getString(6), Class.forName(resultSet.getString(4)).asInstanceOf[Class[AnyRef]]))
-      eventsRows ::= commandRow
+    getQueryResult(SELECT_COMMAND_BY_ID) {statement =>
+      statement.setLong(1, commandId.uid)
+    } { resultSet =>
+      if (resultSet.next()) {
+        CommandRow(
+          CommandId(resultSet.getLong(1)),
+          UserId(resultSet.getLong(2)),
+          resultSet.getTimestamp(3).toInstant,
+          serializer.fromJson(resultSet.getString(6), Class.forName(resultSet.getString(4)).asInstanceOf[Class[AnyRef]]))
+      } else {
+        throw new IllegalStateException("Command not found " + commandId)
+      }
     }
-
-    statement.close()
-    connection.close()
-    eventsRows.reverse
   }
 
 }
