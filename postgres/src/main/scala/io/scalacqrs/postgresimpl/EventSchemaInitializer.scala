0510a45df63a722a4c12a31090be4123ee36ec25
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-04-24 16:59:51 +0200

    First draft of function for inserting undo event

--- postgres/src/main/scala/io/scalacqrs/postgresimpl/EventSchemaInitializer.scala
+++ postgres/src/main/scala/io/scalacqrs/postgresimpl/EventSchemaInitializer.scala
@@ -16,6 +16,7 @@ class EventSchemaInitializer(implicit dbDataSource: DataSource)  {
       case e: Exception => () //ignore until CREATE SEQUENCE IF NOT EXISTS is available in PostgreSQL
     }
     createAddEventFunction()
+    createAddUndoEventFunction()
   }
 
 
@@ -80,6 +81,36 @@ class EventSchemaInitializer(implicit dbDataSource: DataSource)  {
         |	RAISE EXCEPTION 'Concurrent aggregate modification exception, command id %, user id %, aggregate id %, expected version %, current_version %, event_type %, event_type_version %, event %', command_id, user_id, aggregate_id, expected_version, current_version, event_type, event_type_version, event;
         |    END IF;
         |    INSERT INTO events (id, command_id, user_id, aggregate_id, event_time, version, event_type, event_type_version, event) VALUES (NEXTVAL('events_seq'), command_id, user_id, aggregate_id, current_timestamp, current_version + 1, event_type, event_type_version, event);
+        |    UPDATE aggregates SET version = current_version + 1 WHERE id = aggregate_id;
+        |END;
+        |$$
+        |LANGUAGE 'plpgsql' VOLATILE;
+      """.stripMargin)
+  }
+
+
+  private def createAddUndoEventFunction(): Unit = {
+    executeStatementWithoutParams(
+      """
+        |CREATE OR REPLACE FUNCTION add_undo_event(command_id bigint, user_id bigint, aggregate_id bigint, expected_version INT, aggregate_type VARCHAR(128), event_type VARCHAR(128), event_type_version INT, event VARCHAR(10240), undo_count INT)
+        |RETURNS void AS
+        |$$
+        |DECLARE
+        |    current_version int;
+        |BEGIN
+        | SELECT aggregates.version INTO current_version from aggregates where id = aggregate_id;
+        |    IF NOT FOUND THEN
+        |        IF expected_version = 0 THEN
+        |            RAISE EXCEPTION 'Cannot undo event for non existing aggregate';
+        |        ELSE
+        |    	    RAISE EXCEPTION 'aggregate not found, id %, aggregate_type %', aggregate_id, aggregate_type;
+        |        END IF;
+        |    END IF;
+        |    IF current_version != expected_version THEN
+        |	RAISE EXCEPTION 'Concurrent aggregate modification exception, command id %, user id %, aggregate id %, expected version %, current_version %, event_type %, event_type_version %, event %', command_id, user_id, aggregate_id, expected_version, current_version, event_type, event_type_version, event;
+        |    END IF;
+        |    INSERT INTO events (id, command_id, user_id, aggregate_id, event_time, version, event_type, event_type_version, event) VALUES (NEXTVAL('events_seq'), command_id, user_id, aggregate_id, current_timestamp, current_version + 1, event_type, event_type_version, event);
+        |    INSERT INTO noop_events(id, from_version) VALUES (CURRVAL(events_seq), current_version + 1);
         |    UPDATE aggregates SET version = current_version + 1 WHERE id = aggregate_id;
         |END;
         |$$
